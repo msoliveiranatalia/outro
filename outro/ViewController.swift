12bbefc3b5baa08d1cba11655707613fc85bdacb
nmso <nmso@MacBook-Pro-de-nmso.local>
2022-07-04 10:27:15 -0300

    Adiciona projeto do xcode e realiza primeiro commit

---
+++ outro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  outro
+//
+//  Created by nmso on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
